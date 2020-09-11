419661ff91c639cf2d7fc91d2921a1853f084d01
sanchitofficial07 <70484213+sanchitofficial07@users.noreply.github.com>
2020-09-11 15:37:14 +0530

    Add files via upload

---
+++ Day 4/Question 1/index.js
@@ -0,0 +1,18 @@
+function japan() {
+  const japanimg = document.getElementsByTagName("img");
+  let newUrl =
+    "https://d36tnp772eyphs.cloudfront.net/blogs/1/2011/05/japan.jpg";
+  japanimg[0].src = newUrl;
+}
+function paris() {
+  const parisimg = document.getElementsByTagName("img");
+  let newUrl =
+    "https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+  parisimg[0].src = newUrl;
+}
+function london() {
+  const londonimg = document.getElementsByTagName("img");
+  let newUrl =
+    "https://cdn.londonandpartners.com/assets/73295-640x360-london-skyline-ns.jpg";
+  londonimg[0].src = newUrl;
+}
