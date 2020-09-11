20061268c67ed8e8f0678f48c5f1f654c042f76f
sanchitofficial07 <70484213+sanchitofficial07@users.noreply.github.com>
2020-09-11 15:39:20 +0530

    Add files via upload

---
+++ Day 4/Question 3/index.js
@@ -0,0 +1,28 @@
+let array=[
+    {
+        name:"Alice",
+        age:23,
+        country:"India",
+        hobbies:["Cooking","Painting","Coding"],
+    },
+    {
+        name:"Mark",
+        age:33,
+        country:"America",
+        hobbies:["Playing Guitar","Cricket","Coding"],
+    },
+    {
+        name:"Martin",
+        age:30,
+        country:"Korea",
+        hobbies:["Scoccer","Hacking","Riding"],
+    }
+
+];
+
+
+function arrayOfObjects(){
+console.log(array);
+}
+
+arrayOfObjects();
